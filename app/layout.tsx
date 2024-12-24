f1a9c2b549b0f2cb575e616a642ee08e0920b92a
RAJI-Zakaria <zakariaraji.me@gmail.com>
2024-12-25 00:03:52 +0100

    initiating the components [right & left bar]

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { LeftBar, RightBar } from "@/components";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-green-300`}
       >
-        {children}
+        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
+          <div className="px-2 xsm:px-4 xxl:px-8 ">
+            <LeftBar />
+          </div>
+          <div className="flex-1 lg:min-w-[600px] bg-red-400 border-x-[1px] border-borderGray ">
+            {children}
+          </div>
+          <div className="hidden lg:flex ml-4 md:ml-8 flex-1 ">
+            <RightBar />
+          </div>
+        </div>
       </body>
     </html>
   );
